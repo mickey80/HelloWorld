8c10d6f8a63550aae2a71d88da77a3bfc112ae97
mickey80 <mickp_83@hotmail.com>
2015-03-18 13:15:53 -0400

    Initial commit

---
+++ com.devmickey/src/main/java/com/example/HelloModule.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Main {
+    static int a = 1;
+    static int b = 2;
+    public static void main (String[] args) {
+    System.out.println("hello! " + a);
+    System.out.println("hello! " + a);
+
+    }
+
+}
